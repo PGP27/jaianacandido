0584c07503d869988f5ff6805fb2e4b9c77403fb
PGP27 <peg.gep27@gmail.com>
2022-07-17 11:57:45 -0300

    update

--- src/components/Locations/components/LocationCard.tsx
+++ src/components/Locations/components/LocationCard.tsx
@@ -3,16 +3,17 @@ interface LocationCardProps {
   title: string,
   desc: string,
   mapURL?: string,
+  imgRounded?: boolean,
 };
 
-const LocationCard = ({ image, title, desc, mapURL }: LocationCardProps) => (
+const LocationCard = ({ image, title, desc, mapURL, imgRounded }: LocationCardProps) => (
   <div className="max-w-[400px] lg:max-w-none my-10 mx-4 xl:mx-10 flex-1 flex flex-col items-center bg-slate-50 shadow-lg border rounded-lg py-4 px-8">
     <p className="h-20 text-lg text-center">{title}</p>
     <div className="h-full flex flex-col">
-      <img src={image} alt={title} className="h-28 self-center my-8" />
+      <img src={image} alt={title} className={`h-28 self-center my-8 ${imgRounded && 'rounded-full'}`} />
       <p className="h-40 sm:h-28">{desc}</p>
       {mapURL && (
-        <a className="underline underline-offset-2 hover:text-blue-800 transition" rel="noreferrer" href={mapURL} target="_blank">Ver no mapa</a>
+        <a className="w-fit underline underline-offset-2 hover:text-blue-800 transition" rel="noreferrer" href={mapURL} target="_blank">Ver no mapa</a>
       )}
     </div>
   </div>
--- src/components/Locations/index.tsx
+++ src/components/Locations/index.tsx
@@ -1,5 +1,5 @@
 import fratelli from '../../assets/fratelli.png';
-import ioci from '../../assets/ioci.png';
+import ser from '../../assets/ser.jpg';
 import online from '../../assets/online.png';
 import SessionTitle from "../SessionTitle";
 import LocationCard from "./components/LocationCard";
@@ -11,14 +11,15 @@ const Locations = () => (
       <LocationCard
         image={fratelli}
         title="Clínica de Psicologia Fratelli"
-        desc="Rua Vicente Nogueira Braga, 214 - Sala 801 - Fátima, Fortaleza - CE, 60040-570"
+        desc="Rua Vicente Nogueira Braga, 214 - Sala 501 - Fátima, Fortaleza - CE, 60040-570"
         mapURL="https://www.google.com.br/maps/place/Cl%C3%ADnica+de+Psicologia+Fratelli/@-3.7557435,-38.5243352,17z/data=!3m1!4b1!4m5!3m4!1s0x7c7491a345f6e39:0xd50429de08063c48!8m2!3d-3.7557839!4d-38.5221426"
       />
        <LocationCard
-        image={ioci}
-        title="IOCI - Instituto de Odontologia e Ciências Integradas"
-        desc="Rua Julz Francisco Augusto de Oliveira, 17 - Patriolino Ribeiro, Fortaleza - CE, 60810-260"
-        mapURL="https://www.google.com.br/maps/place/IOCI+-+Instituto+de+Odontologia+e+Ci%C3%AAncias+Integradas/@-3.7604319,-38.4900761,17z/data=!3m1!4b1!4m5!3m4!1s0x7c748a62bca22d1:0x55ea762fdca1b057!8m2!3d-3.7604373!4d-38.4878874"
+        image={ser}
+        imgRounded
+        title="Espaço Ser"
+        desc="Avenida Desembargador Moreira, 2800 - 1º andar, sala 101 - Aldeota, Fortaleza - CE, 60170-172"
+        mapURL="https://www.google.com.br/maps/place/Santo+Amaro+Empresarial/@-3.7500861,-38.5038942,18z/data=!3m1!4b1!4m5!3m4!1s0x7c749eb66ef4209:0xcd15263c19fcdb35!8m2!3d-3.7500861!4d-38.5031204"
       />
        <LocationCard
         image={online}
